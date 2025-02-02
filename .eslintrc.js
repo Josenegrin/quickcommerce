d1cfe283e0ac919e1b08e197fe9e16161c63417e
Jose Negrin <soyjosenegrin@gmail.com>
2025-02-02 20:45:07 +0100

    chore: avoid import error

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,23 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.json'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+      alias: {
+        map: [
+          ['@src', './src'],
+          ['@components', './src/components'],
+          ['@screens', './src/screens'],
+          ['@utils', './src/utils'],
+          ['@services', './src/services'],
+          ['@store', './src/store']
+        ],
+        extensions: ['.js', '.jsx', '.json']
+      }
+    }
+  },
 };
